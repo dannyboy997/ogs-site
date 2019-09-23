7fd8eb0b9217f008dcfa5444fe52db3be7834b12
Daniel Hagen <dannyboy997@outlook.com>
2019-09-23 15:06:22 -0700

    Initial react site

---
+++ ogs-home/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to Open Game Studios (OGS). We are a small game studio powered and driven by a community of developers and gamers.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
